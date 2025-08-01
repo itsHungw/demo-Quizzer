b8734c28950867c82ba9e9e9f830d643882c4df6
NguyenVinhHungSE <vinhung06vl@gmail.com>
2025-08-02 00:10:05 +0700

    video 17

--- src/App.js
+++ src/App.js
@@ -3,28 +3,43 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
-  const person = {
-    name: 'Nguyen Vinh Hung',
-    age: 19,
+import React from 'react';
 
+class App extends React.Component {
+  state = {
+    name: 'Nguyen Vinh Hung',
+    birth: 2006,
+
+  };
+  render() {
+    return (
+      <div>My name is {this.state.name} and im {2026 - this.state.birth} year old</div>
+    )
   }
-  return (
-    <MyComponent />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello World my name is {person.name}, Im {person.age} year old.
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+//   const person = {
+//     name: 'Nguyen Vinh Hung',
+//     age: 19,
+
+//   }
+//   return (
+//     <MyComponent />
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Hello World my name is {person.name}, Im {person.age} year old.
+//     //     </p>
+//     //     <div>Count = {count}</div>
+//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//     //   </header>
+//     // </div>
+//   );
+// }
 
 export default App;
