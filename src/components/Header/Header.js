1e7fab70909fa96aa1dc321f3e37a97da0a4fab4
NguyenVinhHungSE <vinhung06vl@gmail.com>
2025-08-04 00:14:38 +0700

    video 50

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -12,9 +12,9 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link className='nav-link' to="/">Home</Link>
-                        <Link className='nav-link' to="/users">User</Link>
-                        <Link className='nav-link' to="/admins">Admin</Link>
+                        <NavLink className='nav-link' to="/">Home</NavLink>
+                        <NavLink className='nav-link' to="/users">User</NavLink>
+                        <NavLink className='nav-link' to="/admins">Admin</NavLink>
 
                         {/* <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="users">User</Nav.Link>
