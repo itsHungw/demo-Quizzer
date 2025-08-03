62974dd9d2c5da349eccb29fedb52d1d8bdf28b5
NguyenVinhHungSE <vinhung06vl@gmail.com>
2025-08-03 21:06:14 +0700

    video 47

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
+
 import './App.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React, { useState } from 'react';
-import AddUserInfo from './components/AddUserInfo';
-import DisplayInfo from './components/DisplayInfo';
+import Header from './components/Header/Header';
 const App = () => {
 
 
   return (
     <>
-      <div>Hello world</div>
-      <button>hiii</button>
+      <div className='app-container'>
+
+        <Header />
+      </div>
     </>
   )
 
