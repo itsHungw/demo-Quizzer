a0fea4904ab8d8628b8bc0fb40722a24e7819f4b
NguyenVinhHungSE <vinhung06vl@gmail.com>
2025-08-11 20:25:01 +0700

    persitgate

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
@@ -15,14 +15,20 @@ import Auth from './components/Auth/Login';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Layout from './Layout';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
 
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+
+      {/* <RootComponent /> */}
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+
+    </PersistGate>
 
   </Provider>
 );
