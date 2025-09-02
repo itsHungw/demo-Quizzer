8edefc7727cb16d3a7e4c14e639c07dc967e1cd1
NguyenVinhHungSE <vinhung06vl@gmail.com>
2025-09-03 04:58:32 +0700

    bug translation

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,26 +1,16 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+import enTranslation from './locales/en/translation.json';
+import viTranslation from './locales/vi/translation.json';
 
 i18n
-    // i18next-http-backend
-    // loads translations from your server
-    // https://github.com/i18next/i18next-http-backend
-    .use(Backend)
-    // detect user language
-    // learn more: https://github.com/i18next/i18next-browser-languageDetector
-    .use(LanguageDetector)
-    // pass the i18n instance to react-i18next.
     .use(initReactI18next)
-    // init i18next
-    // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
-        debug: true,
+        resources: {
+            en: { translation: enTranslation },
+            vi: { translation: viTranslation },
+        },
+        lng: 'en',
         fallbackLng: 'en',
         interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
+            escapeValue: false,
         },
     });
-
-export default i18n;
