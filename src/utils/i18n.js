033c2041621d2648981b9a13c6cf493f1e536f36
NguyenVinhHungSE <vinhung06vl@gmail.com>
2025-09-03 05:05:20 +0700

    fix translation

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,3 +1,8 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+
 i18n
     .use(Backend)
     .use(LanguageDetector)
@@ -12,3 +17,6 @@ i18n
             loadPath: process.env.PUBLIC_URL + '/locales/{{lng}}/{{ns}}.json',
         },
     });
+
+
+export default i18n;
